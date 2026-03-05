f3b694edb3675d35da9499341f758cc0a32f6171
Iwansal64 <iwansal64@gmail.com>
2026-03-05 21:20:05 +0700

    [improve] Better spacing and fix mobile navbar background

--- src/components/homepage/NavbarMobileTrigger.tsx
+++ src/components/homepage/NavbarMobileTrigger.tsx
@@ -8,8 +8,8 @@ export default function Navbar() {
       }
 
       return (
-            <div className="flex flex-row justify-end">
-                  <button className="relative w-12 aspect-square cursor-pointer md:hidden" onClick={toggleNavbar}>
+            <div className="flex flex-row justify-end bg-black py-4 px-8">
+                  <button className="relative w-8 aspect-square cursor-pointer md:hidden" onClick={toggleNavbar}>
                         <span className={`pointer-events-none absolute left-0 bg-white w-full h-1 duration-250 ${isOpen ? "-rotate-45 top-1/2 -translate-y-1/2" : "top-0"}`}></span>
                         <span className={`pointer-events-none absolute left-0 bg-white w-full h-1 duration-250 ${isOpen ? "rotate-45" : "rotate-0"} top-1/2 -translate-y-1/2`}></span>
                         <span className={`pointer-events-none absolute left-0 bg-white w-full h-1 duration-250 ${isOpen ? "rotate-45 top-1/2 -translate-y-1/2" : "top-full -translate-y-full"}`}></span>
